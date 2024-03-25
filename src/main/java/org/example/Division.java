a5b1af68c95dad1fbea0e26cb869edaa4a5b88cf
Matteo <kalzmatteo@gmail.com>
2024-03-25 15:26:40 +0100

    first push

---
+++ src/main/java/org/example/Division.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Division {
+    public int getResult(int n1, int n2) {
+        if (n2 == 0){
+            return -1;
+        } else{
+            return (n1/n2);
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Division division = new Division();
+        System.out.println(division.getResult(10, 2));
+    }
+}
